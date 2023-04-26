f6423c27b8fe1f5608279f889dade09f8edb5170
João Victor <jvdso.15@gmail.com>
2023-04-26 16:34:47 -0300

    feat - Adição da opção de se conectar com o bd em nuvem

--- DB/Connection.hs
+++ DB/Connection.hs
@@ -17,6 +17,9 @@ localDB = defaultConnectInfo {
     connectPort = 5432
 }
 
+connectCloud :: IO Connection
+connectCloud = connectPostgreSQL "host=ep-autumn-smoke-494458.us-east-2.aws.neon.tech port=5432 user=rodrigo.rodrigues password=RrAOk4gU9QED dbname=nokia-3310 sslmode=require options=project=ep-autumn-smoke-494458"
+
 connectionMyDB :: IO Connection
 connectionMyDB = connect localDB
 
@@ -24,6 +27,7 @@ connectionMyDB = connect localDB
 startDatabase:: IO Connection
 startDatabase = do
  c <- connectionMyDB
+--  c <- connectCloud  conectar com o bd em nuvem
  createChips c
  createCalls c
  createMessages c
