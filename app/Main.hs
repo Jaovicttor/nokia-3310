7fda0e3530499ffd0054298dc07ecf3f7d5488cb
thallesVasconcelos <thallesvasconcelos5@gmail.com>
2023-05-09 19:19:57 -0300

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Database.PostgreSQL.Simple
-import DB.Connection
+import DB.Connection 
 import DB.Models.Chip
 import DB.Models.Contact
 import DB.Models.Call
-import DB.Models.Event
+import DB.Models.Event 
 import DB.Models.Message
 import DB.Models.Alarm
-
-main :: IO()
-main = do
- putStrLn "Criando base de dados..."
- startDatabase 
- putStrLn "Base de dados criada"
- 
---  insertChip "Joao Victor" "79988686084" False
+import App.Modules.Alarm.Index
+import App.Modules.Alarm.MainAlarm
+import Control.Concurrent
+main :: IO()     
+main = do 
+    --startDatabase
+    _ <- forkIO alarm      
+    mainAlarm      
+          
+--  putStrLn "Criando base de dados..." 
+--  putStrLn "Base de dados criada"  
+            
+--  insertChip "Joao Victor" "79988686084" False   
 --  insertChip "Rodrigo Correia" "83954478512" False
 --  insertContact "Rodrigo Monstrinho" "83954478512" "2023-01-01" (-1) 1
 --  insertMessage "Eae corno" "2023-04-26 16:09:12" 1 2
@@ -23,11 +28,11 @@ main = do
 --  insertEvent "Aniversario do monstrinho" "2023-04-26" "" 1
 --  insertAlarm "15:50" "Teste alarms"
 
-startDatabase:: IO()
-startDatabase = do  
- createChips 
- createCalls 
- createMessages 
+startDatabase:: IO()      
+startDatabase = do     
+ createChips           
+ createCalls    
+ createMessages   
  createEvents 
  createContacts 
- createAlarms
+ createAlarms     
---
+++ app/modules/alarm/mainAlarm.hs
@@ -0,0 +1,23 @@
+module App.Modules.Alarm.MainAlarm where
+import Data.Time.Format
+import Data.Time.LocalTime
+import App.Shared.Main.Helper.Timer
+import DB.Models.Alarm
+import DB.Models.Chip
+import Data.Time
+import Control.Concurrent
+import Control.Monad
+
+alarm :: IO ()
+alarm = forever $ do 
+        verificationAlarm
+        threadDelay 1000000
+
+verificationAlarm :: IO ()
+verificationAlarm = do
+        timeString <- getCurrentTimeString
+        let parsedTime = parseTimeM True defaultTimeLocale "%H:%M" timeString :: Maybe TimeOfDay
+        alarmes <- case parsedTime of
+              Just time -> verificationAlarms time
+              Nothing -> return []
+        mapM_ (\(Alarm id time title active chip_id) -> putStrLn $ "\n" ++ "Alarme disparado: " ++ show time ++ "\t" ++ title) alarmes
