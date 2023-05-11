b32643ffae64e04250445ae1758f79a838a0c8a7
João Victor <jvdso.15@gmail.com>
2023-05-11 08:43:33 -0300

    .

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import qualified App.Modules.Alarm.Index as Alarm
 import qualified App.Modules.Calendar.Index as Calendar
 import qualified App.Modules.Contacts.Index as Contacts
 import qualified App.Modules.Message.Index as Message
-import App.Modules.Call.Index (menu)
+import qualified App.Modules.Call.Index as Call
 import Control.Concurrent 
 import qualified App.Shared.Main.Helper.Display as Display
  
@@ -13,11 +13,11 @@ import Control.Concurrent
 
 main :: IO ()
 main = do  
-    Display.printeHeader "--------"
+    Display.printeHeader "---------"
     putStrLn "1 - Contatos"   
     putStrLn "2 - Ligações"
     putStrLn "3 - Mensagens" 
-    putStrLn "4 - Calendário"
+    putStrLn "4 - Calendário"   
     putStrLn "5 - Alarmes"
     putStrLn "0 - Sair"
     Display.printeBottom
@@ -27,7 +27,7 @@ main = do
     else do
         case choice of
             "1" -> Contacts.main  
-            "2" -> menu 
+            "2" -> Call.menu 
             "3" -> Message.mainMessage
             "4" -> Calendar.menuCalendar
             "5" -> Alarm.mainAlarm
